e379bb82f3fba6e22db2adcaa824a115be454789
Marcin Nocuń <marnoc24@gmail.com>
2026-06-11 08:48:08 +0200

    Zmiana nazwy VM

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-stack-server"
+  name                = "vm-stack-server10"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_D2s_v3"
